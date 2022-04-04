079f2e396e0bbb08cfb9bdccfd63008a1b46e68d
Subham Saha <89207473+subhams56@users.noreply.github.com>
2022-04-04 22:42:34 +0530

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,87 +1,22 @@
-from collections import deque
+# Using a Python dictionary to act as an adjacency list
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
 
-# to keep track of the blocks of maze
-class Grid_Position:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+visited = set() # Set to keep track of visited nodes of graph.
 
-# each block will have its own position and cost of steps taken
-class Node:
-    def __init__(self, pos: Grid_Position, cost):
-        self.pos = pos
-        self.cost = cost
+def dfs(visited, graph, node):  #function for dfs 
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
 
-def create_node(x, y, c):
-    val = Grid_Position(x, y)
-    return Node(val, c + 1)
-
-#dfs algo for maze
-def dfs(Grid, dest: Grid_Position, start: Grid_Position):
-    adj_cell_x = [1, 0, 0, -1]
-    adj_cell_y = [0, 1, -1, 0]
-    m, n = (len(Grid), len(Grid))
-    visited_blocks = [[False for i in range(m)]
-               for j in range(n)]
-    visited_blocks[start.x][start.y] = True
-    stack = deque()
-    sol = Node(start, 0)
-    stack.append(sol)
-    neigh = 4
-    neighbours = []
-    cost = 0
-    while stack: 
-        current_block = stack.pop()
-        current_pos = current_block.pos
-        if current_pos.x == dest.x and current_pos.y == dest.y:
-            print("Algorithm used = DFS")
-            print("Path found!!")
-            print("Total nodes visited = ", cost)
-            return current_block.cost
-        x_pos = current_pos.x
-        y_pos = current_pos.y
-     
-        for i in range(neigh):
-            if x_pos == len(Grid) - 1 and adj_cell_x[i] == 1:
-                x_pos = current_pos.x
-                y_pos = current_pos.y + adj_cell_y[i]
-            if y_pos == 0 and adj_cell_y[i] == -1:
-                x_pos = current_pos.x + adj_cell_x[i]
-                y_pos = current_pos.y
-            else:
-                x_pos = current_pos.x + adj_cell_x[i]
-                y_pos = current_pos.y + adj_cell_y[i]
-            if x_pos != 12 and x_pos != -1 and y_pos != 12 and y_pos != -1:
-                if Grid[x_pos][y_pos] == 1:
-                    if not visited_blocks[x_pos][y_pos]:
-                        cost += 1
-                        visited_blocks[x_pos][y_pos] = True
-                        stack.append(create_node(x_pos, y_pos, current_block.cost))
-    return -1
-
-def main():
-    maze = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0],
-            [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0],
-            [0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1],
-            [0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-            [0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-            [0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-            [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
-            [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-    destination = Grid_Position(10, 0)
-    starting_position = Grid_Position(4, 11)
-    res2 = dfs(maze, destination, starting_position)
-    if res2 != -1:
-        print("Steps with backtracking = ", res2)
-    else:
-        print("Path does not exit")
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print("main start\n")
-    main()
-
-
+# Driver Code
+print("Following is the Depth-First Search")
+dfs(visited, graph, '5')
